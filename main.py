e5aba9b76b530ccd8a4a2a0ea12582526456a550
itsf4llofstars <90528743+itsf4llofstars@users.noreply.github.com>
2022-11-17 04:36:17 +0000

    Add initial main.py file and test code

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""main.py file"""
+
+
+def say_hi():
+    """say_hi"""
+    print("Hello, world!")
+
+
+def main():
+    """main function"""
+    say_hi()
+
+
+if __name__ == "__main__":
+    main()
