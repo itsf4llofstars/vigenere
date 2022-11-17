b574b3bd960b4062265982bfad66c286b0b5f2e5
itsf4llofstars <90528743+itsf4llofstars@users.noreply.github.com>
2022-11-17 05:13:09 +0000

    Add set_key method

--- vigenere.py
+++ vigenere.py
@@ -5,11 +5,11 @@ from collections import deque
 class Vigenere:
     """Vigenere class"""
     def __init__(self,key: str) -> None:
-        letters = [
+        self.letters = [
             'A', 'B', 'C', 'D', 'E,' 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
         ]
-        rotor = deque([
+        self.rotor = deque([
             'A', 'B', 'C', 'D', 'E,' 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
         ])
@@ -17,6 +17,10 @@ class Vigenere:
         self.message = ''
         self.encoded_message = ''
 
+    def set_key(self):
+        """Sets rotor[0] equal to key[0]"""
+        while self.rotor[0] != self.key[0]:
+            deque.rotate(self.rotor, -1)
 
 def main():
     """main function"""
